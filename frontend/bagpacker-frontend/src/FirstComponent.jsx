70b9dc56cd0e56470826ef97dc8bf7f57b562283
Aarush Pandey <aarushp2005@gmail.com>
2024-12-17 16:39:01 +1030

    completed react tutorial, actual dev stuff started

---
+++ frontend/bagpacker-frontend/src/FirstComponent.jsx
@@ -0,0 +1,5 @@
+function FirstComponent() {
+    return <h1>Hello, world! Welcome to this React app!</h1>;
+}
+
+export default FirstComponent
--- frontend/bagpacker-frontend/src/main.jsx
+++ frontend/bagpacker-frontend/src/main.jsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App";
+import Profile from "./Profile";
+import ErrorPage from "./ErrorPage";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "profile/:name",
+    element: <Profile />,
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
-)
+);
